e2fb83a5e9e5038a0f442176b0ec6a2211b6e1fd
feffe1702 <fpm.asd@gmail.com>
2024-10-31 18:31:24 +0100

    [DONE] complete test page with ajax call

--- public/data/post.js
+++ public/data/post.js
@@ -2,31 +2,31 @@ const posts = [
     {
       titolo: 'Introduzione al giardinaggio',
       contenuto: 'In questo post, esploreremo i fondamenti del giardinaggio...',
-      img: './images/giardinaggio.jpg',
+      img: '../images/giardinaggio.jpg',
       tags: ['giardinaggio', 'piante', 'cura']
     },
     {
       titolo: 'Consigli per la coltivazione di rose',
       contenuto: 'Le rose sono bellissime e richiedono attenzione particolare...',
-      img: './images/rosa.jpg',
+      img: '../images/rosa.jpg',
       tags: ['giardinaggio', 'rose', 'fiori']
     },
     {
       titolo: 'Come affrontare le infestazioni',
       contenuto: 'Scopri i metodi per proteggere il tuo giardino da parassiti...',
-      img: './images/parassiti.jpg',
+      img: '../images/parassiti.jpg',
       tags: ['disinfestazione', 'parassiti', 'consigli']
     },
     {
       titolo: 'Progettare un giardino sostenibile',
       contenuto: 'Impara a rendere il tuo giardino più ecologico...',
-      img: './images/sostenibile.jpg',
+      img: '../images/sostenibile.jpg',
       tags: ['sostenibilità', 'ecologia', 'giardinaggio']
     },
     {
       titolo: "Piante ideali per l'inverno",
       contenuto: 'Alcune piante possono resistere a temperature fredde...',
-      img: './images/piante-invernali.jpg',
+      img: '../images/piante-invernali.jpg',
       tags: ['inverno', 'piante', 'giardinaggio']
     }
   ];
--- public/prova/main.js
+++ public/prova/main.js
@@ -1,6 +1,28 @@
+rowEl = document.querySelector(".row");
 
 axios.get("http://127.0.0.1:3002/posts")
 .then(response => {
     console.log(response);
-    
+    response.data.lista.forEach(element => {
+        let urlImg = element.img;
+        let urlTitle = element.titolo;
+        let urldesc = element.contenuto;
+        let urlTags = element.tags;
+        const card = document.createElement("div");
+        card.classList.add("col-md-5");
+        card.innerHTML = `
+                <div class="card mb-5">
+                    <img src="${urlImg}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">${urlTitle}</h5>
+                      <p class="card-text">${urldesc}</p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        ${urlTags.map(tag => `<li class="list-group-item">${tag}</li>`).join('')}
+                    </ul>
+                </div>
+                    `
+        rowEl.appendChild(card);
+        
+    });
 })
